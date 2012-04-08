623c826f357778f3f131332d46b00b045ac83107
ChaotikmindSrc@gmail.com <ChaotikmindSrc@gmail.com@32ac4dcc-de7a-11de-b980-fbd970685da6>
2012-04-08 00:31:45 +0000

    message overlay code

---
+++ trunk/Client/T4c client/SysMsg.h
@@ -0,0 +1,41 @@
+#ifndef SYSMSG_H
+#define SYSMSG_H
+
+#include "Headers.h"
+
+#include "Global.h"
+#include "d3dfont.h"
+
+class TSysMsg 
+{
+private:
+    TD3dText *Text;
+    float TimeStamp;
+	unsigned long Color;
+public:
+    TSysMsg(void);
+    ~TSysMsg(void);
+    void SetText( const char *Txt, const unsigned long BaseColor,const float Time,const bool MultiLine);
+    void Draw(const int x,const int y ,const unsigned long Alpha);
+    unsigned int GetHeight(void);
+    float GetTimestamp(void){ return TimeStamp; };
+};
+
+class TSysMsgList
+{
+private:
+	std::list<TSysMsg*> MsgList;
+	//critical section to maybe remove later
+	//AFTER in deepth inspection of code
+	CriticalSection Lock;
+public:
+	TSysMsgList(void);
+	~TSysMsgList(void);
+	void AddMessage(const char *Text, unsigned long Color, float Time, bool AllowNewLine);
+	void Render(float DeltaTime);
+};
+
+extern TSysMsgList SysMsgList;
+
+
+#endif
