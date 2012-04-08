66586def34f8ffcc637beacb6971c741ebd540bf
ChaotikmindSrc@gmail.com <ChaotikmindSrc@gmail.com@32ac4dcc-de7a-11de-b980-fbd970685da6>
2012-04-08 00:08:13 +0000

    added a general header

---
+++ trunk/Client/T4c client/Headers.h
@@ -0,0 +1,30 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#pragma warning( disable : 4786 )
+#pragma warning( disable : 4291 )
+
+#include <windows.h>
+#include <WindowsX.h>
+#include <stdlib.h>
+#include <winsock.h>
+
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+#include <stdio.h>
+#include <time.h>
+#define _USE_MATH_DEFINES
+#include <Math.h>
+#include <memory.h>
+#include <process.h>
+
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+#include <queue>
+
+#endif
